7a861132d74a45a8d254cb969aea6024df5a2f79
MuhammadPathan <muhammad.pathan2000@gmail.com>
2021-04-29 16:43:45 +0100

    Add variables
    add image and directory variables

--- CNN.py
+++ CNN.py
@@ -8,3 +8,15 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+#var
+batch_size = 32
+img_height = 224
+img_width = 224
+#3 colour channels
+channels=3
+input_shape=(img_height,img_width,channels)
+#data path
+wd=os.path.abspath(os.getcwd())
+data_folder="data"
+data_dir=os.path.join(wd,data_folder)
