21144c1c1a00d21d70fd4c8b8d79f25836b03603
MuhammadPathan <muhammad.pathan2000@gmail.com>
2021-04-29 16:46:37 +0100

    preprocess and split dataset
    Preprocess images to correct size and split into training and testing set.

--- CNN.py
+++ CNN.py
@@ -20,3 +20,20 @@ input_shape=(img_height,img_width,channels)
 wd=os.path.abspath(os.getcwd())
 data_folder="data"
 data_dir=os.path.join(wd,data_folder)
+
+#load training set
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
