c0f723f36afd72dbdd6487b9228e09886d98f433
MuhammadPathan <muhammad.pathan2000@gmail.com>
2021-04-29 15:28:08 +0100

    import dependencies
    add dependencies  to project

---
+++ CNN.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
